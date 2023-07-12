6d3e19e5f70317c1ba8005c2d07243c330994a8e
flowerside <flowerside@gmail.com>
2023-07-12 10:03:41 +0400

    Initial commit.

---
+++ data-raw/load_datasets.R
@@ -0,0 +1,10 @@
+library(iotc.base.common.data)
+
+EF_RAW = EF.raw()
+usethis::use_data(EF_RAW, overwrite = TRUE)
+
+CA_RAW = CA.raw()
+usethis::use_data(CA_RAW, overwrite = TRUE)
+
+CE_RAW = CE.raw()
+usethis::use_data(CE_RAW, overwrite = TRUE)
