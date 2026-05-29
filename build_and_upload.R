6375d0dbf6755595af9d3945e34776cb2d8969c0
manuchassot <manu.chassot@gmail.com>
2026-05-29 17:36:08 +0400

    Update of CE and improvement of metadata production and export

--- build_and_upload.R
+++ build_and_upload.R
@@ -1,4 +1,43 @@
 library(httr)
 library(iotc.core.utils.misc)
 
+# Load the data on the GitHub repository ####
 source("./data-raw/01_load_datasets.R")
+
+# Generate the datasets ####
+source("./data-raw/02_export_datasets.R")
+
+# TO DO - create converters between legacy and new formats
+# View [Miguel].[dbo].[1NC_WebPageFILE]: RAW | "Z:/Public datasets/2025-01-14/IOTC-DATASETS-2025-01-14-NC-ALL_1950-2023.xlsx"
+# Table [IOTDB].[dbo].[est_NCdissag]: SCI | "Z:/Public datasets/2025-01-14/IOTC-DATASETS-2025-01-14-NC-SCI_1950-2023.xlsx"
+
+# Generate dataset description ###
+setwd("./data-raw/")
+source("03_export_metadata.R")
+setwd("..")
+
+## Prior to disaggregation ####
+
+### Raw dataset ####
+render("./rmd/01_CA_metadata.Rmd",
+       output_dir = paste0("./outputs/html/", as.character(Sys.Date())),
+       output_file = paste0("CA_metadata.html")
+)
+
+### Factorised dataset ####
+
+# Only the main species and gears are considered and all the rest is aggregated under 'UNCL'
+# The factorisation is required to use the IOTC colour palettes (retricted to )
+render("./rmd/02_EF_metadata.Rmd",
+       output_dir = paste0("./outputs/html/", as.character(Sys.Date())),
+       output_file = paste0("EF_metadata.html")
+)
+
+## Best scientific estimates ####s
+render("./rmd/03_CE_metadata.Rmd",
+       output_dir = paste0("./outputs/html/", as.character(Sys.Date())),
+       output_file = paste0("CE_metadata.html")
+)
+
+# Export datasets to Zenodo with Geoflow ####
+# To develop as part of SWIOP
--- data-raw/03_export_metadata.R
+++ data-raw/03_export_metadata.R
@@ -1,6 +1,26 @@
 # Define Data Fields ####
-FIELDS_RC_DATA_TABLE = fread("./RC_FIELDS.csv")[FIELD %in% names(RC_SCI), .(FIELD, DEFINITION, `CODE LIST NAME`, `CODE LIST URL`)]
 
-FIELDS_RC_DATA_TABLE[!is.na(`CODE LIST NAME`), `CODE LIST` := paste0("<a href=\"", `CODE LIST URL`, "\">", `CODE LIST NAME`, "</a>")]
+DATA_FIELDS <- data.table::fread("https://github.com/iotc-secretariat/iotc-glossaries/raw/refs/heads/master/inputs/iotc-dataset-fields.csv")
 
-FIELDS_RC_DATA_TABLE = FIELDS_RC_DATA_TABLE[, -c("CODE LIST NAME", "CODE LIST URL")]
+## Geo-referenced Catches ####
+
+CA_FIELDS <- DATA_FIELDS[DATASET == "CA" & CONTEXT == "GENERAL"]
+
+CA_FIELDS[!is.na(`CODE LIST NAME`), `CODE LIST` := paste0("<a href=\"", `CODE LIST URL`, "\">", `CODE LIST NAME`, "</a>")]
+
+CA_FIELDS <- CA_FIELDS[, -c("CODE LIST NAME", "CODE LIST URL")]
+
+## Geo-referenced Efforts ####
+
+EF_FIELDS <- DATA_FIELDS[DATASET == "EF" & CONTEXT == "GENERAL"]
+
+EF_FIELDS[!is.na(`CODE LIST NAME`), `CODE LIST` := paste0("<a href=\"", `CODE LIST URL`, "\">", `CODE LIST NAME`, "</a>")]
+
+EF_FIELDS <- EF_FIELDS[, -c("CODE LIST NAME", "CODE LIST URL")]
+
+## Georeferenced Catch and Efforts ####
+#CE_FIELDS <- fread("./data/CE_FIELDS.csv")[FIELD %in% names(CA), .(FIELD, DEFINITION, `CODE LIST NAME`, `CODE LIST URL`)]
+
+#CE_FIELDS[!is.na(`CODE LIST NAME`), `CODE LIST` := paste0("<a href=\"", `CODE LIST URL`, "\">", `CODE LIST NAME`, "</a>")]
+
+CE_FIELDS <- CE_FIELDS[, -c("CODE LIST NAME", "CODE LIST URL")]
